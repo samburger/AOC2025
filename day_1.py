e24c28b946a7c67d6d1293a8d6140bcf1f375d51
Sam Friedman <sam.friedman@yale.edu>
2025-12-03 11:36:07 -0500

    part 2, fix edge cases starting from 0

--- day_1.py
+++ day_1.py
@@ -9,7 +9,7 @@ def main_1(input_file):
         pos = (pos + rot) % 100
         if pos == 0:
             zeroes += 1
-    print(zeroes)
+    return zeroes
 
 
 def main_2(input_file):
@@ -20,8 +20,17 @@ def main_2(input_file):
     for rot in rotations:
         dir = -1 if rot.startswith("L") else 1
         rot = int(rot[1:]) * dir
-        pos = (pos + rot) % 100
-        if 
+        zeroes += abs(rot) // 100
+        new_pos = (pos + (rot % 100)) % 100
+        if (new_pos < pos) and rot > 0 and pos != 0:
+            zeroes += 1
+        elif (new_pos > pos) and rot < 0 and pos != 0:
+            zeroes += 1
+        elif new_pos == 0 and pos != 0:
+            zeroes += 1
+        pos = new_pos
+    return zeroes
+
 
 if __name__ == "__main__":
     print("Part 1: ", main_1("01-input.txt"))
